cbada5a1c765f258412b502b2b2171e5af595e2e
Briam Martinez <briammartinez@gmail.com>
2021-04-18 22:18:21 +0200

    feat: created swaggerUi component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1>Home</h1>
+  <div class="home">
+      <SwaggerUi :swaggerObject="testing" />
+  </div>
 </template>
 
 <script>
+import SwaggerUi from '@/components/SwaggerUi.vue';
+import testing from './testing';
+
 export default {
-  name: 'Home',
+  components: { SwaggerUi },
+  beforeMount() {
+    this.testing = testing;
+  },
 };
 </script>
+
+<style lang="css" scoped>
+.Swagger {
+  width: 100%;
+}
+.home {
+  display: flex;
+  height: 100%;
+}
+
+.home  .codemirror,
+.home  .pre {
+    width: 50%;
+    height: 100%;
+    margin: 0;
+    overflow: auto;
+  }
+
+.home  .pre {
+    display: block;
+    padding: 1rem;
+    font-size: 14px;
+    line-height: 1.6;
+    word-break: break-all;
+    word-wrap: break-word;
+  }
+</style>
