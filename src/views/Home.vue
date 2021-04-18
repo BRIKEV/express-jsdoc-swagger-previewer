263f970d7badf1a87f2bcf234b868a9ed9ad5379
Briam Martinez <briammartinez@gmail.com>
2021-04-19 00:11:50 +0200

    feat: added codemirror and navbar into home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,51 @@
 <template>
   <div class="home">
+     <Navbar @onClick="handleClickRun" />
+     <div class="Content">
+      <div class="codemirror">
+        <CodeMirror v-model="code" />
+      </div>
       <SwaggerUi :swaggerObject="testing" />
+     </div>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue';
 import SwaggerUi from '@/components/SwaggerUi.vue';
+import CodeMirror from '@/components/CodeMirror.vue';
 import testing from './testing';
 
 export default {
-  components: { SwaggerUi },
-  beforeMount() {
-    this.testing = testing;
+  components: { Navbar, SwaggerUi, CodeMirror },
+  data() {
+    return {
+      code: '',
+      testing: undefined,
+    };
+  },
+  methods: {
+    handleClickRun() {
+      // TODO: backend api call
+      this.testing = testing;
+    },
   },
 };
 </script>
 
 <style lang="css" scoped>
 .Swagger {
-  width: 100%;
+  width: 50%;
 }
+.codemirror {
+  width: 50%;
+}
+
 .home {
-  display: flex;
+  display: grid;
   height: 100%;
 }
-
-.home  .codemirror,
-.home  .pre {
-    width: 50%;
-    height: 100%;
-    margin: 0;
-    overflow: auto;
-  }
-
-.home  .pre {
-    display: block;
-    padding: 1rem;
-    font-size: 14px;
-    line-height: 1.6;
-    word-break: break-all;
-    word-wrap: break-word;
-  }
+.Content {
+  display: flex;
+}
 </style>
