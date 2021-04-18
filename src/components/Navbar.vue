a4dcba22dc94ba0c57f54950d5b36f5a83a88ab2
Briam Martinez <briammartinez@gmail.com>
2021-04-19 00:10:53 +0200

    feat: initial version of navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav">
+    <img class="brand" src="../assets/images/brand.jpeg" alt="logo" />
+    <button class="nav-button" @click="handleClick">
+      Run
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    handleClick() {
+      this.$emit('onClick');
+    },
+  },
+};
+</script>
+
+<style>
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 15px;
+  border-bottom: 1px solid #DCDCDC;
+}
+.brand {
+  height: 40px;
+}
+.nav-button {
+  cursor: pointer;
+  background: transparent;
+  padding: 15px 7px;
+  border-radius: 0.25rem;
+  border: 1px solid #DCDCDC;
+
+}
+.nav-button:hover {
+    background: #606060;
+    color: #ffF;
+}
+</style>
