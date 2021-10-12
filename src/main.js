1cfffb8db0e66d81bc7d773b659187336efe639e
bri06 <briammartinez@gmail.com>
2021-10-12 23:15:47 +0200

    feat: added examples schemas file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import VueNotification from 'vue-notification';
+import VueMultiselect from 'vue-multiselect';
 import App from './App.vue';
 import router from './router';
 import '@/assets/css/reset.css';
+import 'vue-multiselect/dist/vue-multiselect.min.css';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueNotification);
+Vue.component('VueMultiselect', VueMultiselect);
 
 new Vue({
   router,
