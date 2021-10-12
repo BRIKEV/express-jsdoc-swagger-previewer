e5d6133de36745ea6a54147ab30699205692f5bc
Briam Martinez Escobar <briammartinez@gmail.com>
2021-10-12 21:57:04 +0200

    Feat/ci workflow (#5)
    * chore: added start-server-and-test dependency
    
    * feat: added github action
    
    * feat: created workflows folder
    
    * feat: added pull request template file
    
    * refactor: api file
    
    * chore: updated package-lock

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-export const processOpenapi = ({ payload }) => axios.post(`${process.env.VUE_APP_PREVIEWER_APP_API_URL}/process-openapi`, { payload });
+export const processOpenapi = ({ payload }) => axios.post('/api/v1/process-openapi', { payload });
